938ecba38f3d7b8ce3970178ca7a032a101996d6
SetterUk <Koushik4067@gmail.com>
2026-08-07 11:51:28 +0530

    fix: lower minSdk to 26 for older Android compatibility

--- frontend_android/app/build.gradle.kts
+++ frontend_android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.healthheatv2"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.1"
